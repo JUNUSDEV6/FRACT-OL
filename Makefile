7ae74d5ea0e1759b1bf747c34bc05d5467a6b3d6
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2024-02-05 12:08:57 +0100

    1

---
+++ Makefile
@@ -0,0 +1,25 @@
+NAME = fractol
+SRCS = main.c
+
+OBJS = ${SRCS:.c=.o}
+CC = gcc
+RM = rm -f
+CFLAGS = -Wall -Wextra Werror
+
+%.o: %.c
+	$(CC) -Wall -Wextra -Werror -Imlx -c $< -o $@
+
+$(NAME): $(OBJS)
+	$(CC) $(OBJS) -Lmlx -lmlx -framework OpenGL -framework AppKit -o $(NAME)
+
+all: ${NAME}
+
+clean:
+	${RM} ${OBJS}
+
+fclean: clean
+	${RM} ${NAME}
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/30 11:57:12 by yohanafi          #+#    #+#             */
+/*   Updated: 2024/02/05 12:04:32 by yohanafi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+#include <mlx.h>
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+}
