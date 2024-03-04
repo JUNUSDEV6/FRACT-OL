fe3ff7401e50e507701687aa144405534e192f57
JUNUSDEV6 <younes-hanafi97@hotmail.com>
2024-03-04 11:16:32 +0100

    2

--- event.c
+++ event.c
@@ -6,14 +6,15 @@
 /*   By: yohanafi <yohanafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:53:54 by yohanafi          #+#    #+#             */
-/*   Updated: 2024/02/27 13:51:13 by yohanafi         ###   ########.fr       */
+/*   Updated: 2024/03/04 11:12:46 by yohanafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	ft_close(void)
+int	ft_close(t_fractal *fractal)
 {
+	(void)fractal;
 	exit(0);
 }
 
@@ -24,14 +25,9 @@ int	key_press(int keycode, t_fractal *fractal)
 	return (0);
 }
 
-int	mouse_event(int keycode, t_fractal *fractal)
-{
-	
-}
-
 void	event(t_fractal *fractal)
 {
 	mlx_hook(fractal->mlx_window, 2, 0, key_press, fractal);
 	mlx_hook(fractal->mlx_window, 17, 0, ft_close, fractal);
-	mlx_mouse_hook(fractal->mlx_window, mouse_event, fractal);
+	//mlx_mouse_hook(fractal->mlx_window, mouse_event, fractal);
 }
